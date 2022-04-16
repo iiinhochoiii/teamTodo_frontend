f55f242d70754ba170eb8beca16ada51fdb05aee
dlsgh120 <dlsgh120@gmail.com>
2022-04-16 23:38:06 +0900

    eslint 충돌 나는부분 초기화

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -19,6 +24,10 @@ module.exports = {
       'error',
       {
         endOfLine: 'auto',
+        trailingComma: 'es5',
+        singleQuote: true,
+        semi: true,
+        printWidth: 80,
       },
     ],
     'no-unused-vars': 'off',
