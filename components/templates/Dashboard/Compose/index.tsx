618dceb66ef73f90072de47b25cf32c22918223f
dlsgh120 <dlsgh120@gmail.com>
2022-05-10 23:38:00 +0900

    compose Component 개선중 - Plan (오늘 할일) 부분 작업 완료 - Happend (완료한 일) 에 대한 작업 중

--- components/templates/Dashboard/Compose/index.tsx
+++ components/templates/Dashboard/Compose/index.tsx
@@ -18,16 +18,53 @@ import { useForm } from 'react-hook-form';
 
 const ComposeComponent = () => {
   const [isAdd, setIsAdd] = useState(false);
+  const [isAdd2, setIsAdd2] = useState(false);
   const [isContent, setIsContent] = useState(-1);
   const { register, watch, reset } = useForm();
 
   const [items, setItems] = useState<string[]>([]);
+  const [happendItems, setHappendItems] = useState<string[]>([]);
+  const [allClose, setAllClose] = useState(false);
   const itemRef: React.MutableRefObject<string[]> = useRef([]);
+  const contentRef: React.MutableRefObject<HTMLDivElement | null> =
+    useRef(null);
 
-  const createItems = (title: string) => {
+  useEffect(() => {
+    document.addEventListener('mousedown', clickContentOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', clickContentOutside);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (allClose) {
+      setIsContent(-1);
+    }
+  }, [allClose, items]);
+
+  useEffect(() => {
+    setAllClose(false);
+  }, [isContent]);
+
+  const clickContentOutside = (
+    event: React.BaseSyntheticEvent | MouseEvent
+  ) => {
+    if (contentRef.current && !contentRef.current.contains(event.target)) {
+      setIsAdd(false);
+      setAllClose(true);
+    }
+  };
+
+  const createItems = (title: string, type?: string) => {
     setIsAdd(false);
-    setItems((state) => [...state, title]);
-    reset({ title: '' });
+    if (type === '1') {
+      setItems((state) => [...state, title]);
+      reset({ title: '' });
+    } else {
+      setHappendItems((state) => [...state, title]);
+      reset({ title2: '' });
+    }
   };
 
   const updateItems = (id: number, title: string) => {
@@ -59,10 +96,12 @@ const ComposeComponent = () => {
                 isContent={index === isContent}
                 onClick={() => {
                   setIsContent(index);
+                  setIsAdd(false);
                   reset({
                     title: item,
                   });
                 }}
+                ref={contentRef}
               >
                 <ArticleIcon />
                 <ArticleContent>
@@ -94,7 +133,7 @@ const ComposeComponent = () => {
               </Article>
             ))}
           {isAdd && (
-            <Article isContent={true}>
+            <Article isContent={true} ref={contentRef}>
               <ArticleIcon />
               <ArticleContent>
                 <Box>
@@ -107,7 +146,7 @@ const ComposeComponent = () => {
                       disabled={!watch('title')}
                       onClick={() => {
                         const title = watch('title');
-                        createItems(title);
+                        createItems(title, '1');
                       }}
                     >
                       <AddCircleIcon />
@@ -124,8 +163,101 @@ const ComposeComponent = () => {
           <AddItems
             onClick={() => {
               setIsAdd(true);
+              setIsContent(-1);
               reset({
                 title: '',
+              });
+            }}
+          >
+            <AddItemIcon />
+            <Text font={{ size: 'S', weight: 300 }} color="purple">
+              Add Item
+            </Text>
+          </AddItems>
+        </Box>
+      </Box>
+
+      {/* 완료 */}
+      <Box style={{ margin: '50px 0 0 0' }}>
+        <Text font={{ size: 'M', weight: 600 }}>Happend</Text>
+        <Box style={{ margin: '20px 0 0 0' }}>
+          {happendItems.length > 0 &&
+            happendItems.map((item, index) => (
+              <Article
+                key={index}
+                isContent={index === isContent}
+                onClick={() => {
+                  setIsContent(index);
+                  setIsAdd(false);
+                  reset({
+                    title: item,
+                  });
+                }}
+                ref={contentRef}
+              >
+                <ArticleIcon />
+                <ArticleContent>
+                  {isContent === index ? (
+                    <Box>
+                      <FormTextarea {...register('title2')} />
+                      <ArticleEditor>
+                        <IconButton
+                          disabled={!watch('title2')}
+                          onClick={() => {
+                            const title = watch('title2');
+                            updateItems(index, title);
+                          }}
+                        >
+                          <ChangeCircleIcon />
+                        </IconButton>
+                        <IconButton
+                          disabled={!watch('title2')}
+                          onClick={() => removeItem(index)}
+                        >
+                          <DeleteIcon />
+                        </IconButton>
+                      </ArticleEditor>
+                    </Box>
+                  ) : (
+                    <Text font={{ size: 'S', weight: 300 }}>{item}</Text>
+                  )}
+                </ArticleContent>
+              </Article>
+            ))}
+          {isAdd2 && (
+            <Article isContent={true} ref={contentRef}>
+              <ArticleIcon />
+              <ArticleContent>
+                <Box>
+                  <FormTextarea
+                    placeholder={'What is most important to get done today?'}
+                    {...register('title2')}
+                  />
+                  <ArticleEditor>
+                    <IconButton
+                      disabled={!watch('title2')}
+                      onClick={() => {
+                        const title = watch('title2');
+                        createItems(title, '2');
+                      }}
+                    >
+                      <AddCircleIcon />
+                    </IconButton>
+                    <IconButton disabled={!watch('title2')}>
+                      <DeleteIcon />
+                    </IconButton>
+                  </ArticleEditor>
+                </Box>
+              </ArticleContent>
+            </Article>
+          )}
+
+          <AddItems
+            onClick={() => {
+              setIsAdd2(true);
+              setIsContent(-1);
+              reset({
+                title2: '',
               });
             }}
           >
