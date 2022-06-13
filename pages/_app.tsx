5412d83eeb420c14892f76ca1c52903a0b8242e9
dlsgh120 <dlsgh120@gmail.com>
2022-06-13 22:20:51 +0900

    fix: warning about google font with next/head - _app.tsx(pages) 에서 google font 관련 link를 next/head에 추가한 부분에서 warning 발생 - _document.tsx(pages) 에서 render() 부분에 link로 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,28 +3,13 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from '../styles/global-styles';
 import { theme } from '../styles/theme';
-import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin=""
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lilita+One&family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { ServerStyleSheet } from 'styled-components';
-import Document, { DocumentContext } from 'next/document';
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
 import { RenderPageResult } from 'next/dist/shared/lib/utils';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext): Promise<RenderPageResult> {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<RenderPageResult> {
     // 스타일 구성 요소의 ServerStyleSheet 클래스를 인스턴스화 한다
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
@@ -14,7 +22,8 @@ export default class MyDocument extends Document {
       // renderPage를 커스텀 하는 이유는 서버 측 렌더링에서 제대로 작동하기 위해 애플리케이션을 래핑해야 하는 css-in-js 라이브러리와 함께 사용하기 위한 것
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
         });
 
       const initialProps = await Document.getInitialProps(ctx);
@@ -30,5 +39,21 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lilita+One&family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
