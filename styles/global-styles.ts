459c3a97b3bb65349cdd156495561508637333da
dlsgh120 <dlsgh120@gmail.com>
2023-09-26 18:21:57 +0900

    fix: deploy 후 font 적용 안되는 문제 수정

--- styles/global-styles.ts
+++ styles/global-styles.ts
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Lilita+One&family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
+
   body {
     margin: 0;
     padding: 0;
